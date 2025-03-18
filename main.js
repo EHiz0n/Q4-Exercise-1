d8c33ad239e0bac1591550c29a6d90bd70b67511
EHiz0n <hizon.e-@obmontessori.edu.ph>
2025-03-18 08:44:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+        function round() {
+            var a = document.getElementById('a').value;
+            var b = document.getElementById('b').value;
+            window.alert(Math.round(a));
+        }
+
+        function power() {
+            var a value = document.getElementById("a").value;
+            var b value = document.getElementById("b").value;
+            window.alert(Math.pow(a,b));
+        }
+
+        function square() {
+            var a value = document.getElementById("a").value;
+            var b value = document.getElementById("b").value;
+            window.alert(Math.sqrt(a));
+        }
+
+        function checkEquality() {
+            var a value = document.getElementById("a").value;
+            var b value = document.getElementById("b").value;
+            window.alert(a == b ? "A is equal to B" : "A is NOT equal to B");
+        }
